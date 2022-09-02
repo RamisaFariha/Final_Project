39cf654a6f6066644b32c077b33d7c6f9f17f5c4
RamisaFariha <fariha.ramisa26@gmail.com>
2022-09-02 18:33:21 +0600

    Final Project

---
+++ finalp.js
@@ -0,0 +1,25 @@
+function validate()
+{
+    var username=document.getElementById("username").value;
+    var password=document.getElementById("password").value;
+    if(username=="Ramisa"&&password=="Fariha")
+    {
+        
+        return false;
+    }
+
+else
+{
+    alert("Login failed")
+}
+}
+const cookiecontainer=document.querySelector(".cookies");
+const cookiebutton=document.querySelector(".cookie");
+cookiebutton.addEventListener("click",()=>{
+    cookiecontainer.classList.remove("active");
+    localStorage.setItem("cookieBannerDisplayed","true");
+});
+setTimeout(()=>{
+    if(!localStorage.getItem("cookieBannerDisplayed"))
+    cookiecontainer.classList.add("active");
+}, 2000);
